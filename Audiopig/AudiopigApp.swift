7f07b5a961340d890de0549c27055efa699c33d5
Nitay <nitay290@icloud.com>
2026-06-08 10:57:07 +0300

    Initial Commit

---
+++ Audiopig/AudiopigApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudiopigApp.swift
+//  Audiopig
+//
+//  Created by Nitay A. on 08/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct AudiopigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
